485130ac76b13dcd606cc72ab2d79d5aebc4375a
joseQ190978 <jvqhjose@gmail.com>
2024-09-17 22:15:22 -0700

    fix(aut):cane in controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Request } from 'express';
-import { ActiveUser } from 'src/common/decorators/active-user.decorator';
 import { UserActiveInterface } from 'src/common/interfaces/user-active.interface';
-import { Role } from '../common/enums/rol.enum';
 import { AuthService } from './auth.service';
-import { Auth } from './decorators/auth.decorator';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
 
@@ -30,8 +27,7 @@ export class AuthController {
   @Post('login')
   login(
     @Body()
-    loginDto: LoginDto,
-    @ActiveUser() user: UserActiveInterface
+    loginDto: LoginDto
   ) {
     return this.authService.login(loginDto);
   }
